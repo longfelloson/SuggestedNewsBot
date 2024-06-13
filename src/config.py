9c81848ecb1c79455d145e116afe8a7fe8e7da75
Ivan Kashirin <kashirinivan@icloud.com>
2024-06-13 18:06:06 +0400

    Micro Config Commit

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMINS_IDS = [int(ADMIN_ID) for ADMIN_ID in os.getenv('ADMINS_IDS').split()] if os.getenv('ADMINS_IDS') else []
+ADMINS_IDS = [int(ADMIN_ID) for ADMIN_ID in os.getenv('ADMINS_IDS').split(',')] if os.getenv('ADMINS_IDS') else []
 CHAT_ID = int(os.getenv('CHAT_ID')) if os.getenv('CHAT_ID') else None
 
 DATABASE_URL = os.getenv('DATABASE_URL')
